dfbf20e70aa9ae8e7ef6f729cb88989cbe8903c0
Pacifique001 <tuyizerepacifique053@gmail.com>
2026-04-02 19:33:49 +0200

    My third portfolio code push with solved certificates calling

--- src/app/about/certifications.ts
+++ src/app/about/certifications.ts
@@ -1,9 +1,5 @@
 "use client"
 
-import { Card, CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Award } from "lucide-react"
-
 const certificates = [
   {
     title: "Effective Presentations",
